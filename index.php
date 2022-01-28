ddb363fdd2646f2605ac8ee726be655fb4426ecc
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-01-28 11:04:47 +0100

    fin 1 principe

--- index.php
+++ index.php
@@ -44,4 +44,68 @@ $valeurs = array('01/01/2007','01/01/07');
 foreach ($valeurs as $x) {
 afcher($x,filter_var($x,FILTER_VALIDATE_REGEXP,$options));
 }
+echo "/////////////////////////////////////<br />";
+$texte = "<b>c’est l’été</b>";
+echo "// texte afché sans précaution<br />\n";
+echo $texte,"<br />\n";
+echo "// FILTER_SANITIZE_SPECIAL_CHARS<br />\n";
+echo filter_var($texte,FILTER_SANITIZE_SPECIAL_CHARS),"<br />\n";
+echo "// FILTER_SANITIZE_STRING<br />\n";
+echo filter_var($texte,FILTER_SANITIZE_STRING),"<br />\n";
+
+
+echo "/////////////////////////////////////<br />";
+function aficher($x,$f) { // utilisée pour afcher les résultats
+    echo var_export($x,TRUE),'<br /> => ',var_export($f,TRUE),'<br />';
+    }
+    echo '<b>Filtrer un tableau de nombres entiers</b><br />';
+    $valeurs = array('123','abc');
+    // Même fltre à appliquer à toutes les données,
+    // sans indicateur ni option.
+    aficher($valeurs,filter_var_array($valeurs,FILTER_VALIDATE_INT));
+    echo '<b>Filtrer un tableau de données diverses (1)</b><br />';
+    $valeurs = array
+    (
+    'age' => 123,
+    'taille' => 'abc',
+    'mail' => 'contact@Mickey-MOUSE.fr'
+    );
+    // Filtre diférent mais "simple" (sans indicateur
+    // ni option) à appliquer aux données.
+    $fltres = array
+    (
+    'age' => FILTER_VALIDATE_INT,
+    'taille' => FILTER_VALIDATE_INT,
+    'mail' => FILTER_VALIDATE_EMAIL
+    );
+    aficher($valeurs,filter_var_array($valeurs,$fltres));
+    echo '<b>Filtrer un tableau de données diverses (2)</b><br />';
+    $valeurs = array
+    (
+    'age' => 123,
+    'taille' => 'abc',
+    'mail' => 'contact@Mickey-MOUSE.fr'
+    );
+    // Filtre avec options et indicateur à appliquer à une
+    // des données.
+    $fltre_age = array
+    (
+    'flter' => FILTER_VALIDATE_INT,
+    'options' => array('min_range' => 0,'max_range' => 100),
+    'fags' => FILTER_NULL_ON_FAILURE
+    );
+    // Noter la mention d'un fltre pour une donnée
+    // qui n'existe pas.
+    $fltres = array
+    (
+    'age' => $fltre_age,
+    'taille' => FILTER_VALIDATE_INT,
+    'poids' => FILTER_VALIDATE_INT, // n'existe pas
+    'mail' => FILTER_VALIDATE_EMAIL
+    );
+    aficher($valeurs,filter_var_array($valeurs,$fltres));
+    // Désactiver l'ajout des éléments vides.
+    echo '<b>La même chose en désactivant l\'ajout d\'éléments vides</b>
+    <br />';
+    aficher($valeurs,filter_var_array($valeurs,$fltres,FALSE));
 ?>
